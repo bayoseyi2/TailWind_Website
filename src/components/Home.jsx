eb1ddac101cae5003db8af0b8b16a8ee1509c73e
bee <bee>
2023-06-18 14:20:21 -0300

    intro my name

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,19 @@ import {HiArrowNarrowRight} from 'react-icons/hi'
 
  const Home = () => {
   return (
-    <div>Home</div>
+    <div name='home' className='w-full h-screen bg-[#0a19]'>
+        
+     {/* Container */}
+     <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <p>Hi,my name is </p>
+        <h1>Bayo ADEOLA</h1>
+        <p>I'm a Data Analyst and a Full Stack Developer. I am passionate about solving business questions and assisting in decision making based on data analysis and visualization.
+            I love web and mobile application development using React frameworks, Node.js and javascript. 
+            For data analysis, SQL, Python, Power BI, Ms SQL Server, Power Query, Tableau are some of the tools.
+        </p>
+     </div>
+        
+        </div>
   )
 }
 

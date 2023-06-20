a01a54b5db4dfef76dd805c51810cd2bd272f82f
bee <bee>
2023-06-19 21:45:23 -0300

    node

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ app.use(express.static("build", OPTIONS));
 app.use(express.static(path.join(__dirname, "./build")));
 
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./build", "index.html"));
   });

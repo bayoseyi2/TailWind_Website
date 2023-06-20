d476ab2fec4aff5a197494b3b2759e6125cb935a
bee <bee>
2023-06-19 22:03:52 -0300

    fixed layout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ import {Link} from 'react-scroll'
     <div name='home' className='w-full h-screen bg-[#bdc4bd99]'>
         
      {/* Container */}
-     <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+     <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center'>
        
         <p className='text-yellow-600'>Hi,my name is </p>
         <h1 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>Bayo ADEOLA</h1>

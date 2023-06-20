3342ffecf71f21f9d4ac8fee2c36eeba9be96e7c
bee <bee>
2023-06-19 21:34:22 -0300

    build

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ const OPTIONS = {
   redirect: false
 };
 app.use(express.static("build", OPTIONS));
-// app.use(express.static(path.join(__dirname, "./build")));
+app.use(express.static(path.join(__dirname, "./build")));
 
 
-// app.get("/*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "./build", "index.html"));
-//   });
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./build", "index.html"));
+  });

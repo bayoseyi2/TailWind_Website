dd2cd843eccf7af0f9a3112d186fd6f184055689
bee <bee>
2023-06-25 10:37:46 -0300

    Footbar

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Work from "./components/Work";
 import Contact from "./components/Contact";
-import Footbar from "./components/Footbar";
+
 
 
 function App() {
